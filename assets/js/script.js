e235c49c6d7afe3944e09f8a8106afcbb5fc020d
Devin Simpson <82175278+0xZ3n@users.noreply.github.com>
2022-08-15 21:27:27 -0400

    added edge case for empty fields, added reset()

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,14 @@ var taskFormHandler = function(event) {
     console.dir(taskNameInput);
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    //check if inputs are empty
+    if(!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
